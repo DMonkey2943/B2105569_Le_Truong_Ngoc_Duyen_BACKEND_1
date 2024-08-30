fb6159479cd0f62ebd901fc3b69e3db809d8b420
DMonkey2943 <ltnduyen29042003@gmail.com>
2024-08-30 13:56:47 +0700

    define routes for Contact

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
+const contactRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
+app.use("api/contacts", contactRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application!" });
 });
 
 module.exports = app;
-
-
-
